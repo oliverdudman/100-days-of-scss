35e421cc4e2ec552e4441e859d3dc95196978688
Oliver Dudman <oliver.dudman@icloud.com>
2018-06-10 16:23:23 +0100

    add transitions day 13

--- days/day13/app/js/main.js
+++ days/day13/app/js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-  
+  $(".tile__cover").on("click", function() {
+    $(".contact").addClass("active");
+  });
+  $(".contact__close").on("click", function() {
+    $(".contact").removeClass("active");
+  })
 });
